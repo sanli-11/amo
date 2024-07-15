751e166e6d76480836c97e8e428ae757c5d1dec3
SanLi <sanli.has11@gmail.com>
2024-07-16 02:03:56 +0500

    Installed PropTypes and Added Logo to Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import Logo from "../assets/svg/Logo.jsx";
+
+const Header = () => {
+  return (
+    <div className="bg-black/50 px-8 py-3">
+      <Logo />
+    </div>
+  )
+}
+
+export default Header
